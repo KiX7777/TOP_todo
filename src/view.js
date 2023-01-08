909fc1c7a2788f4cfe6a29b85e2efc1eee92e625
KiX7777 <kristianboi202@gmail.com>
2023-01-08 20:41:48 +0100

    priority show

--- src/view.js
+++ src/view.js
@@ -137,12 +137,24 @@ class View {
   createTaskCard(task) {
     let card = document.createElement('div');
     card.classList = 'taskCard card';
+    let shortDate = task.dueDate.slice(0, 13);
+
     card.innerHTML = `    
-                      <p class="taskCardinfo title">${task.title}</p>
+                      <p class="taskCardinfo title"><strong>${
+                        task.title
+                      }</strong> ${
+      task.priority === 'important'
+        ? `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 important">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+      </svg>
+      `
+        : ''
+    }</p>
                       <p class="taskCardinfo">${task.description}</p>
-                      <p class="taskCardinfo">${task.dueDate}</p>
-                      <p class="taskCardinfo">${task.priority}</p>
+                      <p class="taskCardinfo"><strong>Due date: </strong>${shortDate}</p>
+                      <p class="taskCardinfo"></p>
                       <p class="taskCardinfo">${task.notes}</p>
+                      
                       <div class="cardBtns ">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
